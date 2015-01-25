bd59ac680a6e7e5e3b33d9328466415b04642b7a
harri feng <harrifeng@gmail.com>
2015-01-25 16:22:21 +0800

    Use Annotation to set url
    run this example with :
    http://localhost:8080/first.view?name=fenghaoran

---
+++ src/main/java/org/example/FirstServlet.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/first.view")
+public class FirstServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1> Hello!" + name + " !</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+        //super.doGet(request, response);
+    }
+}
